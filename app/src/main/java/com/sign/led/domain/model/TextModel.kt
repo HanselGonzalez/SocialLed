79df43ca3568e70988a3e75b040e50953f3fae70
Hansel Gonzalez <hansel.espino10@gmail.com>
2024-03-18 10:58:39 -0600

    view and text with resizable perfect, background working perfect, animation without speed

---
+++ app/src/main/java/com/sign/led/domain/model/TextModel.kt
@@ -0,0 +1,17 @@
+package com.sign.led.domain.model
+
+import android.graphics.Typeface
+import android.view.animation.Animation
+
+data class TextModel (
+
+    val text:String,
+    val size:Float,
+    val typeface: Typeface,
+    val color:Int,
+    val animation: Animation?
+
+
+
+
+)
