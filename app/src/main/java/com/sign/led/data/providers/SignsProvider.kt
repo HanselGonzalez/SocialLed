94334a1f2d8ee2d2615628efd062c87dfe3ac0f8
Hansel Gonzalez <hansel.espino10@gmail.com>
2024-04-01 11:51:38 -0600

    Icon App

--- app/src/main/java/com/sign/led/data/providers/SignsProvider.kt
+++ app/src/main/java/com/sign/led/data/providers/SignsProvider.kt
@@ -1,5 +1,6 @@
 package com.sign.led.data.providers
 
+import android.content.Context
 import com.sign.led.domain.model.ItemViewFullModelInfo
 import com.sign.led.domain.model.ItemViewFullModelInfo.SignFour
 import com.sign.led.domain.model.ItemViewFullModelInfo.SignOne
@@ -7,23 +8,23 @@ import com.sign.led.domain.model.ItemViewFullModelInfo.SignThree
 import com.sign.led.domain.model.ItemViewFullModelInfo.SignTwo
 import javax.inject.Inject
 
-class SignsProvider @Inject constructor() {
+class SignsProvider @Inject constructor(private val context:Context) {
 
     fun getSignsProvider():List<ItemViewFullModelInfo>{
         return listOf(
-            SignOne,
-            SignTwo,
-            SignThree,
-            SignFour
+            SignOne(context),
+            SignTwo(context),
+            SignThree(context),
+            SignFour(context)
         )
     }
 
     fun getSignProviderById(id:Long):ItemViewFullModelInfo{
         return when(id){
-            1L -> SignOne
-            2L -> SignTwo
-            3L -> SignThree
-            4L -> SignFour
+            1L -> SignOne(context)
+            2L -> SignTwo(context)
+            3L -> SignThree(context)
+            4L -> SignFour(context)
             else -> null!!
         }
     }
--- app/src/main/java/com/sign/led/domain/model/ItemViewFullModelInfo.kt
+++ app/src/main/java/com/sign/led/domain/model/ItemViewFullModelInfo.kt
@@ -1,28 +1,128 @@
 package com.sign.led.domain.model
 
+import android.content.Context
 import com.sign.led.R
 
 sealed class ItemViewFullModelInfo (val id:Long, val name:Int, val backgroundColor:Int, val backgroundImage:Boolean, val listText:List<TextModel>){
 
-    data object SignOne:ItemViewFullModelInfo(1,R.string.welcome, -2354116,false, listText = listOf(
-        TextModel(1,"WELCOME",131.25f, "fugazone.ttf", -1, R.anim.anim_float_text, 2000, 0.12446536f,0.28564453f),
-        TextModel(2,"WELCOME",131.25f, "fugazone.ttf", -16316375, R.anim.anim_float_text, 2000, 0.12446536f,-0.00953311f),
-        TextModel(3,"WELCOME",131.25f, "fugazone.ttf", -16316375, R.anim.anim_float_text, 2000, 0.12446536f,0.5730329f)
+    data class SignOne(private val context: Context) : ItemViewFullModelInfo(
+        1, R.string.welcome, -2354116, false, listText = listOf(
+            TextModel(
+                1,
+                context.getString(R.string.welcome_caps),
+                110.25f,
+                "fugazone.ttf",
+                -1,
+                R.anim.anim_float_text,
+                2000,
+                0.12446536f,
+                0.28564453f
+            ),
+            TextModel(
+                2,
+                context.getString(R.string.welcome_caps),
+                110.25f,
+                "fugazone.ttf",
+                -16316375,
+                R.anim.anim_float_text,
+                2000,
+                0.12446536f,
+                -0.00953311f
+            ),
+            TextModel(
+                3,
+                context.getString(R.string.welcome_caps),
+                110.25f,
+                "fugazone.ttf",
+                -16316375,
+                R.anim.anim_float_text,
+                2000,
+                0.12446536f,
+                0.5730329f
+            )
 
-    ))
+        )
+    )
 
-    data object SignTwo:ItemViewFullModelInfo(2,R.string.study_time, -11788936,false, listText = listOf(
-        TextModel(1,"Study time",131.25f, "dmseriftext.ttf", -1, R.anim.anim_blink, 700, 0.113896236f,0.3460984f),
-        TextModel(2,"Study time",99.60388f, "dmseriftext.ttf", -16316375, R.anim.anim_blink, 700, 0.19565052f,0.03348214f)
-        ))
+    data class SignTwo(private val context: Context) : ItemViewFullModelInfo(
+        2, R.string.study_time, -11788936, false, listText = listOf(
+            TextModel(
+                1,
+                context.getString(R.string.study_time),
+                131.25f,
+                "dmseriftext.ttf",
+                -1,
+                R.anim.anim_blink,
+                700,
+                0.113896236f,
+                0.3460984f
+            ),
+            TextModel(
+                2,
+                context.getString(R.string.study_time),
+                99.60388f,
+                "dmseriftext.ttf",
+                -16316375,
+                R.anim.anim_blink,
+                700,
+                0.19565052f,
+                0.03348214f
+            )
+        )
+    )
 
-    data object SignThree:ItemViewFullModelInfo(3,R.string.motivation, -16777216,false, listText = listOf(
-        TextModel(1,"You are destined for something",81.33911f, "leaguegothic.ttf", -2448096, R.anim.anim_none, 0, 0.13715526f,0.26832217f),
-        TextModel(2,"try and find it",52.5f, "dmsan.ttf", -2448096, R.anim.anim_none, 0, 0.32597408f,0.6603423f)))
+    data class SignThree(private val context: Context) : ItemViewFullModelInfo(
+        3, R.string.motivation, -16777216, false, listText = listOf(
+            TextModel(
+                1,
+                context.getString(R.string.welcome_phrase_motivation_1),
+                81.33911f,
+                "leaguegothic.ttf",
+                -2448096,
+                R.anim.anim_none,
+                0,
+                0.13715526f,
+                0.26832217f
+            ),
+            TextModel(
+                2,
+                context.getString(R.string.welcome_phrase_motivation_2),
+                52.5f,
+                "dmsan.ttf",
+                -2448096,
+                R.anim.anim_none,
+                0,
+                0.32597408f,
+                0.6603423f
+            )
+        )
+    )
 
-    data object SignFour:ItemViewFullModelInfo(4,R.string.live_streaming, -13151254,false, listText = listOf(
-        TextModel(1,"Live Streaming",131.25f, "leaguegothic.ttf", -1, R.anim.anim_blink, 1000, 0.15818922f,0.26506695f),
-        TextModel(2,"0",131.25f, "leaguegothic.ttf", -65536, R.anim.anim_blink, 1000, 0.7858006f,0.27297246f)
-        ))
+    data class SignFour(private val context: Context) : ItemViewFullModelInfo(
+        4, R.string.live_streaming, -13151254, false, listText = listOf(
+            TextModel(
+                1,
+                context.getString(R.string.live_streaming),
+                131.25f,
+                "leaguegothic.ttf",
+                -1,
+                R.anim.anim_blink,
+                1000,
+                0.15818922f,
+                0.26506695f
+            ),
+            TextModel(
+                2,
+                context.getString(R.string.live_streaming2),
+                131.25f,
+                "leaguegothic.ttf",
+                -65536,
+                R.anim.anim_blink,
+                1000,
+                0.7858006f,
+                0.27297246f
+            )
+        )
+    )
 
 }
